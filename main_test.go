1a0453d18119e4af8c9dc1233d5422ee4e5949a1
Andy Zhang <andyzhng@gmail.com>
2019-03-17 22:49:52 +0800

    add benchmark for key function Analyse

--- main_test.go
+++ main_test.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/binary"
+	"io/ioutil"
+	"math/rand"
 	"testing"
 )
 
-func TestBuildHeap(t *testing.T) {
+func Benchmark_Analyse(b *testing.B) {
+	file, err := ioutil.TempFile("", "benchmark")
+	if err != nil {
+		b.Fatal(err)
+	}
+	writeBuf := bufio.NewWriter(file)
+	var buf = make([]byte, 8)
+	for i := 0; i < 10*1000*1000; i++ {
+		binary.BigEndian.PutUint64(buf, uint64(rand.Int31()))
+		writeBuf.Write(buf)
+	}
+	writeBuf.Flush()
+	file.Close()
+	Analyse(10*1000*1000, file.Name(), []float32{0.1, 0.05, 0.01})
+}
+
+func Test_BuildHeap(t *testing.T) {
 	var data = make([]byte, 2048, 2048)
 	for j := 0; j < 2; j++ {
 		for i := 0; i < 1024/8; i++ {
